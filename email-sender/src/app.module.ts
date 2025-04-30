a92a529fb0a44968514ed531870347785e2375b6
rezatoosi <reza.toosi@gmail.com>
2025-04-30 19:43:26 +0330

    add nest/config, get mailerModule settings by config module

--- email-sender/src/app.module.ts
+++ email-sender/src/app.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.SMTP_SERVER || "localhost",
-        port: 1025,
-      },
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>("SMTP_SERVER", "localhost"),
+          port: configService.get<number>("SMTP_PORT", 1025),
+        },
+      }),
+      inject: [ConfigService]
     }),
   ],
   controllers: [AppController],
