65e6659078b0ef15d199c909faba1fad060efa69
rezatoosi <reza.toosi@gmail.com>
2025-04-30 20:34:39 +0330

    use configService for getting rabbitmq server url in microservie bootstrap

--- email-sender/src/main.ts
+++ email-sender/src/main.ts
@@ -1,17 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AsyncOptions, MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  const app = await NestFactory.createMicroservice<AsyncOptions<MicroserviceOptions>>(
     AppModule,
     {
-      transport: Transport.RMQ,
-      options: {
-        urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'], //TODO: get by config module
-        queue: 'daily_sales_report',
-      },
-    },
+      useFactory: (configService: ConfigService) => (
+        {
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_SERVER_URL', 'amqp://localhost:5672')],
+            queue: configService.get<string>('daily_sales_report'),
+          },
+        }
+      ),
+      inject: [ConfigService]
+    }
   );
   await app.listen();
 }
